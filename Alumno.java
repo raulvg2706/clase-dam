6050044d10e7fb776bcd3359e2276158bbee77d1
raulvg <valbuenagarciaraul@gmail.com>
2024-01-17 13:28:56 +0100

    Nuevo commit 2

--- Alumno.java
+++ Alumno.java
@@ -1,5 +1,5 @@
 public class Alumno {
-    
+
     // el nombre completo del alumno
     private String nombre;
     // el numero de matricula
@@ -11,18 +11,15 @@ public class Alumno {
      * Crea un alumno nuevo
      */
     public Alumno(String nombreCompleto, String numeroMatriculaAlumno, int edadAlumno) {
-            nombre = nombreCompleto;
-            numeroMatricula = numeroMatriculaAlumno;
-            edad = edadAlumno;
+        nombre = nombreCompleto;
+        numeroMatricula = numeroMatriculaAlumno;
+        edad = edadAlumno;
         if (nombreCompleto.length() < 3) {
             System.err.println("Error: La longitud del nombre del alumno debe ser al menos tres caracteres.");
         }
         if (numeroMatriculaAlumno.length() < 4) {
             System.err.println("Error: La longitud del número de matrícula del alumno debe ser al menos cuatro caracteres.");
         } 
-        else {
-            numeroMatricula = numeroMatriculaAlumno;
-        }
     }
 
     /**
@@ -31,26 +28,30 @@ public class Alumno {
     public String getNombre() {
         return nombre;
     }
-    
+
     /**
      * Cambia el nombre del alumno
      */
     public void cambiarnombre(String nuevoNombre) {
         nombre = nuevoNombre;
     }
-    
+
     /**
      * Imprime por pantalla los detalles del alumno
      */
     public void imprimeDetalles() {
         System.out.println(nombre + " (" + numeroMatricula + ") - " + edad + " aÃ±os");
     }
-    
+
     /**
      * Devuelve el nombre de usuario que el alumno debe configurar
      * en su cuenta de Github en formato de 7 caracateres
      */
     public String getNombreUsuarioGithub() {
         return nombre.substring(0, 3) + numeroMatricula.substring(0, 4);
+    }
+    
+    public boolean esMayorDeEdad() {
+        return edad >= 18;
     }
 }
